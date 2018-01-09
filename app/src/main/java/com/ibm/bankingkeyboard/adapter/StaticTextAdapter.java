37a8205d38045b85d3ddd2729022b2eed5b74ecd
Sabyasachi <biswasab@in.ibm.com>
2018-01-09 16:20:24 +0530

    initial commit

---
+++ app/src/main/java/com/ibm/bankingkeyboard/adapter/StaticTextAdapter.java
@@ -0,0 +1,17 @@
+package com.ibm.bankingkeyboard.adapter;
+
+import android.content.Context;
+
+import com.ibm.bankingkeyboard.TextKeyboardService;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class StaticTextAdapter extends BaseTextAdapter {
+
+    public StaticTextAdapter(Context context, String[] emojiTextsAsStrings, ArrayList<Integer> iconIds) {
+        super((TextKeyboardService) context);
+        this.emojiTexts =  new ArrayList<String>(Arrays.asList(emojiTextsAsStrings));
+        this.iconIds = iconIds;
+    }
+}
